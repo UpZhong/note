3dc0c3c2dd7d1f7d136726d1e7b10fc6323b1ed9
zfred <18782958987@163.com>
2019-06-20 17:41:19 +0800

    希尔算法

--- 算法/shell.mjs
+++ 算法/shell.mjs
@@ -2,3 +2,26 @@
 import {
     Util
 } from './util.mjs';
+
+function shellSort(arr, n) {
+    for (let i = Math.trunc(n / 2); i > 0; i = Math.trunc(i / 2)) {
+        for (let j = i; j < n; j++) {
+            for (let k = j - i; k >= 0; k = k - i) {
+                if (arr[k] > arr[i + k]) {
+                    let value = arr[k];
+                    arr[k] = arr[i + k];
+                    arr[i + k] = value
+                } else {
+                    break
+                }
+            }
+        }
+    }
+}
+
+let arr = [8, 9, 1, 7, 2, 3, 5, 4, 6, 0];
+let n = arr.length;
+shellSort(arr, n);
+console.log(arr)
+
+// node --experimental-modules shell.mjs
